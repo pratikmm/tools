2845715e72a1aef4b90ca12a6a8e17e012dc099b
Isaac Faber <isaacfab@gmail.com>
2018-09-21 17:01:57 -0700

    updating shiny and rstudio

--- plumber/packages.R
+++ plumber/packages.R
@@ -1,7 +1,10 @@
 #Script for common package installation on MatrixDS docker image
-p<-c('gmp','rJava','partitions','Rmpfr','RJDBC','binGroup','mailR',
-	'rgl')
+p<-c('tidyverse','ggplot2','plotly','shiny','shinydashboard','devtools','FinCal','googleVis','DT',
+     'htmlwidgets','rmarkdown','lubridate','leaflet','sparklyr','magrittr','openxlsx',
+     'packrat','roxygen2','knitr','readr','readxl','stringr','broom','feather',
+     'forcats','testthat','plumber','RCurl','rvest','mailR','nlme','foreign','lattice',
+     'expm','Matrix','flexdashboard','caret','mlbench','plotROC','tidyverse','RJDBC','rgdal')
 
 install.packages(p,dependencies = TRUE)
 
-.libPaths( c( .libPaths(), "/srv/R/library") )
+.libPaths( c( .libPaths(), "/home/plumber/R/library") )
