0e3c06c8817f47aa503076a901c33e1822885c59
Isaac Faber <isaacfab@gmail.com>
2018-09-27 15:16:29 -0700

    plumber and shiny updates

--- plumber/entrypoint.sh
+++ plumber/entrypoint.sh
@@ -2,11 +2,11 @@
 
 mkdir -p /home/plumber/plumber
 mkdir -p /home/plumber/R/library
-touch /homoe/plumber/plumber.R
+touch /home/plumber/plumber.R
 
 [ -f  /home/plumber/.Rprofile ] || echo '.libPaths("/home/plumber/R/library/")' > /home/plumber/.Rprofile
 [ -f  /home/plumber/.Renvron ] || echo 'R_LIBS=/usr/local/lib/R/site-library:/usr/local/lib/R/library:/usr/lib/R/library:/home/plumber/R/library
 ' > /home/plumber/.Renvron
 
 Rscript /home/plumber/plumber/plumber.R
-
+/init
--- plumber/plumber.R
+++ plumber/plumber.R
@@ -1,37 +1,16 @@
-##############################################
-# Slate API scripts here
-# This is a working script for the API tools
-##############################################
+########################################################
+# API scripts here
+# update this script for your own API on matrixds.com
+########################################################
 
 # plumber.R
-#* Echo back the input
-#* @param msg The message to echo
-#* @get /slate
-function(){
-  d<-data.frame(uZeeMeeID='d9b3314c-dbfc-4018-90c2-e2bebdf2b4b5',uSlateID='4d313431-b0c8-49f9-87dd-ed86e81166e0')
-  d2<-data.frame(uZeeMeeID='dofdsb3314c-gbfc-403538-90c2-e2be353252b4b5',uSlateID='ase3431-te0c8-4ere9-87werw-ederewwr166e0')
-  d<-rbind(d,d2)
-  l<-list()
-  l$records<-d
-  return(l)
-}
 
-#{   u’row’: [   {   u’ZeeMeeID’: u’d9b3314c-dbfc-4018-90c2-e2bebdf2b4b5',
-#                   u’SlateID’: u'4d313431-b0c8-49f9-87dd-ed86e81166e0'}]}
-
-#* Echo back the input
-#* @param msg The message to echo
-#* @get /echo
-function(msg=""){
-  list(msg = paste0("The messagsfdsafase is: '", msg, "'"))
-}
 
 #* Plot a histogram
 #* @png
-#* @get /plot
-function(){
-  rand <- rnorm(100)
-  hist(rand)
+#* @get /test
+function()
+  return('It is alive!')
 }
 
 #* Return the sum of two numbers
